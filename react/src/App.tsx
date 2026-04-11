991e2a0346aafcd4916737806eb259edd3e1b4e0
momode12 <heritianajulien12@gmail.com>
2026-04-11 16:04:22 +0300

    un peu de config

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,9 +3,11 @@
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+     <div className="min-h-screen bg-chat-bg flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-chat-primary">
+        Tailwind v4 est opérationnel ✅
+      </h1>
+    </div>
   )
 }
 
