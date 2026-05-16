46d214d2523a85abec4dfa644bb46025ac1f4694
momode12 <heritianajulien12@gmail.com>
2026-05-16 16:58:47 +0300

    changeent d'heure dans models users

--- flask/models/user_models.py
+++ flask/models/user_models.py
@@ -1,4 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+
+EAT = timezone(timedelta(hours=3))
+
 
 def create_user_collection(db):
     if "users" not in db.list_collection_names():
@@ -15,6 +18,6 @@ def user_schema(name: str, email: str, hashed_password: str) -> dict:
         "password":   hashed_password,
         "avatar":     None,
         "role":       "user",
-        "created_at": datetime.utcnow(),
-        "updated_at": datetime.utcnow(),
+        "created_at": datetime.now(EAT),
+        "updated_at": datetime.now(EAT),
     }
